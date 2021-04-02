2827d67d8e61bac241940c479c2f25d6bc516a8b
dhershman1 <dustinh17@gmail.com>
2021-04-02 00:12:18 -0400

    fixed spacing

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -8,9 +8,9 @@ if (mods or script.active_mods)['Tree-Nuke'] then
       energy_required = 10,
       ingredients =
       {
-          {"coal", settings.startup['tree_nuke_coal'].value},
-          {"sulfur", settings.startup['tree_nuke_sulfur'].value},
-          {"iron-plate", settings.startup['tree_nuke_plates'].value}
+        {"coal", settings.startup['tree_nuke_coal'].value},
+        {"sulfur", settings.startup['tree_nuke_sulfur'].value},
+        {"iron-plate", settings.startup['tree_nuke_plates'].value}
       },
       result = "tree-nuke"
     }
